715501e8ad6969ff067aa950407da15eb689d72d
ngtrungh1eu <hieuntse160479@fpt.edu.vn>
2023-03-15 23:26:01 +0700

    new

--- ViewsWeb/src/java/Registration/ProductsDAO.java
+++ ViewsWeb/src/java/Registration/ProductsDAO.java
@@ -78,64 +78,7 @@ public class ProductsDAO implements Serializable {
         }
         return null;
     }
-
-//     public ProductsDTO getProductByid(int id){
-//         ArrayList<ProductsDTO> list;
-//         PreparedStatement stm = null;
-//         ResultSet rs = null;
-//         ProductsDTO result = null;  
-//         try {
-//             Connection con = DBHelper.getConnection();
-//             if (con != null) {
-//                 String sql = "select * from products join Categories on products.CateID = Categories.CateID ";
-//                 String where = " where ";
-//                 if (brandValue != null) {
-//                     sql += where;
-//                     sql += " BrandName = ? ";
-//                     where = " and ";
-//                 }
-//                 if (searchValue != null) {
-//                     sql += where;
-//                     sql += " [Name] like ? ";
-//                     where = " and ";
-//                 }
-//                 if (typeValue != null) {
-//                     sql += where;
-//                     sql += " Type  = ? ";
-//                     where = " and ";
-//                 }
-//                 stm = con.prepareStatement(sql);
-//                 int index = 1;
-//                 if (brandValue != null) {
-//                     stm.setString(index, brandValue);
-//                     index++;
-//                 }
-//                 if (searchValue != null) {
-//                     stm.setString(index, "%" + searchValue + "%");
-//                     index++;
-//                 }
-//                 if (typeValue != null) {
-//                     stm.setString(index, typeValue);
-//                     index++;
-//                 }
-//                 rs = stm.executeQuery();
-//                 while (rs.next()) {
-//                     int product_id = rs.getInt("ID");
-//                     String product_name = rs.getString("Name");
-//                     double price = rs.getDouble("Price");
-//                     String image = rs.getString("Image");
-//                     int cateID = rs.getInt("CateID");
-//                     String type = rs.getString("Type");
-//                     int saleof = rs.getInt("SaleOff");
-//                     result = new ProductsDTO(product_id, product_name, price, image, cateID, type, saleof);
-//                     list.add(result);
-//                 }
-//                 return list;
-//             }
-//         } catch (SQLException e) {
-//         }
-//         return null;
-//     }
+    
     public ProductsDTO getProduct(String pId) {
 
         ProductsDTO p = null;
